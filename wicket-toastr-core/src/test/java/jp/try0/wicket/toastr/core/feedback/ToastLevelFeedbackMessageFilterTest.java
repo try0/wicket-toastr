dc4d169ce983e591064b09a4be84fd7dbc449738
Ryo Tsunoda <try0.development@gmail.com>
2018-09-13 13:05:59 +0900

    Add ToastLevelFeedbackMessageFilter's static method tests.

--- wicket-toastr-core/src/test/java/jp/try0/wicket/toastr/core/feedback/ToastLevelFeedbackMessageFilterTest.java
+++ wicket-toastr-core/src/test/java/jp/try0/wicket/toastr/core/feedback/ToastLevelFeedbackMessageFilterTest.java
@@ -1,6 +1,6 @@
 package jp.try0.wicket.toastr.core.feedback;
 
- import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.feedback.FeedbackMessage;
@@ -14,7 +14,6 @@ import jp.try0.wicket.toastr.core.Toast.FeedbackMessageLevel;
 import jp.try0.wicket.toastr.core.Toast.ToastLevel;
 import jp.try0.wicket.toastr.core.test.AbstractToastrTest;
 
-
 /**
  * {@link ToastLevelFeedbackMessageFilter} Tests.
  *
@@ -24,41 +23,152 @@ import jp.try0.wicket.toastr.core.test.AbstractToastrTest;
 public class ToastLevelFeedbackMessageFilterTest extends AbstractToastrTest {
 
 	/**
-	 * {@see ToastLevelFeedbackMessageFilter#lessThan(ToastLevel)
+	 * {@link ToastLevelFeedbackMessageFilter#lessThan(ToastLevel)} test.
 	 *
 	 * @param level
 	 */
 	@ParameterizedTest
-	@EnumSource(value=ToastLevel.class, mode = Mode.EXCLUDE, names= {"UNDEFINED"})
+	@EnumSource(value = ToastLevel.class, mode = Mode.EXCLUDE, names = { "UNDEFINED" })
 	public void lessThan(ToastLevel level) {
 		ToastLevelFeedbackMessageFilter filter = ToastLevelFeedbackMessageFilter.lessThan(level);
 
 		Component dummy = new WebMarkupContainer("dummy");
 
 		Integer feedbackMessageLevel = FeedbackMessageLevel.fromToastLevel(level).toInteger();
-		FeedbackMessage argLevelMessage = new FeedbackMessage(dummy, Toast.create(level, "dummy"), feedbackMessageLevel);
+		FeedbackMessage argLevelMessage = new FeedbackMessage(dummy, Toast.create(level, "dummy"),
+				feedbackMessageLevel);
 		assertFalse(filter.accept(argLevelMessage), "Argument's level is must not contains for accepts level");
-
 
 		Toast successToast = Toast.success("dummy");
 		FeedbackMessage successMessage = new FeedbackMessage(dummy, successToast, FeedbackMessage.SUCCESS);
 		assertEquals(filter.accept(successMessage), successToast.getToastLevel().lessThan(level));
 
-
 		Toast infoToast = Toast.info("dummy");
 		FeedbackMessage infoMessage = new FeedbackMessage(dummy, infoToast, FeedbackMessage.INFO);
 		assertEquals(filter.accept(infoMessage), infoToast.getToastLevel().lessThan(level));
 
-
 		Toast warnToast = Toast.warn("dummy");
 		FeedbackMessage warnMessage = new FeedbackMessage(dummy, warnToast, FeedbackMessage.WARNING);
 		assertEquals(filter.accept(warnMessage), warnToast.getToastLevel().lessThan(level));
-
 
 		Toast errorToast = Toast.error("dummy");
 		FeedbackMessage errorMessage = new FeedbackMessage(dummy, errorToast, FeedbackMessage.ERROR);
 		assertEquals(filter.accept(errorMessage), errorToast.getToastLevel().lessThan(level));
 
 	}
+
+	/**
+	 * {@link ToastLevelFeedbackMessageFilter#lessThanOrEqual(ToastLevel)} test.
+	 *
+	 * @param level
+	 */
+	@ParameterizedTest
+	@EnumSource(value = ToastLevel.class, mode = Mode.EXCLUDE, names = { "UNDEFINED" })
+	public void lessThanOrEqual(ToastLevel level) {
+		ToastLevelFeedbackMessageFilter filter = ToastLevelFeedbackMessageFilter.lessThanOrEqual(level);
+
+		Component dummy = new WebMarkupContainer("dummy");
+
+		Integer feedbackMessageLevel = FeedbackMessageLevel.fromToastLevel(level).toInteger();
+		FeedbackMessage argLevelMessage = new FeedbackMessage(dummy, Toast.create(level, "dummy"),
+				feedbackMessageLevel);
+		assertTrue(filter.accept(argLevelMessage), "Argument's level is must contains for accepts level");
+
+		Toast successToast = Toast.success("dummy");
+		FeedbackMessage successMessage = new FeedbackMessage(dummy, successToast, FeedbackMessage.SUCCESS);
+		assertEquals(filter.accept(successMessage),
+				successToast.getToastLevel().lessThan(level) || successToast.getToastLevel().equals(level));
+
+		Toast infoToast = Toast.info("dummy");
+		FeedbackMessage infoMessage = new FeedbackMessage(dummy, infoToast, FeedbackMessage.INFO);
+		assertEquals(filter.accept(infoMessage),
+				infoToast.getToastLevel().lessThan(level) || infoToast.getToastLevel().equals(level));
+
+		Toast warnToast = Toast.warn("dummy");
+		FeedbackMessage warnMessage = new FeedbackMessage(dummy, warnToast, FeedbackMessage.WARNING);
+		assertEquals(filter.accept(warnMessage),
+				warnToast.getToastLevel().lessThan(level) || warnToast.getToastLevel().equals(level));
+
+		Toast errorToast = Toast.error("dummy");
+		FeedbackMessage errorMessage = new FeedbackMessage(dummy, errorToast, FeedbackMessage.ERROR);
+		assertEquals(filter.accept(errorMessage),
+				errorToast.getToastLevel().lessThan(level) || errorToast.getToastLevel().equals(level));
+
+	}
+
+	/**
+	 * {@link ToastLevelFeedbackMessageFilter#greaterThan(ToastLevel)} test.
+	 *
+	 * @param level
+	 */
+	@ParameterizedTest
+	@EnumSource(value = ToastLevel.class, mode = Mode.EXCLUDE, names = { "UNDEFINED" })
+	public void greaterThan(ToastLevel level) {
+		ToastLevelFeedbackMessageFilter filter = ToastLevelFeedbackMessageFilter.greaterThan(level);
+
+		Component dummy = new WebMarkupContainer("dummy");
+
+		Integer feedbackMessageLevel = FeedbackMessageLevel.fromToastLevel(level).toInteger();
+		FeedbackMessage argLevelMessage = new FeedbackMessage(dummy, Toast.create(level, "dummy"),
+				feedbackMessageLevel);
+		assertFalse(filter.accept(argLevelMessage), "Argument's level is not must contains for accepts level");
+
+		Toast successToast = Toast.success("dummy");
+		FeedbackMessage successMessage = new FeedbackMessage(dummy, successToast, FeedbackMessage.SUCCESS);
+		assertEquals(filter.accept(successMessage), successToast.getToastLevel().greaterThan(level));
+
+		Toast infoToast = Toast.info("dummy");
+		FeedbackMessage infoMessage = new FeedbackMessage(dummy, infoToast, FeedbackMessage.INFO);
+		assertEquals(filter.accept(infoMessage), infoToast.getToastLevel().greaterThan(level));
+
+		Toast warnToast = Toast.warn("dummy");
+		FeedbackMessage warnMessage = new FeedbackMessage(dummy, warnToast, FeedbackMessage.WARNING);
+		assertEquals(filter.accept(warnMessage), warnToast.getToastLevel().greaterThan(level));
+
+		Toast errorToast = Toast.error("dummy");
+		FeedbackMessage errorMessage = new FeedbackMessage(dummy, errorToast, FeedbackMessage.ERROR);
+		assertEquals(filter.accept(errorMessage), errorToast.getToastLevel().greaterThan(level));
+
+	}
+
+	/**
+	 * {@link ToastLevelFeedbackMessageFilter#greaterThanOrEqual(ToastLevel)} test.
+	 *
+	 * @param level
+	 */
+	@ParameterizedTest
+	@EnumSource(value = ToastLevel.class, mode = Mode.EXCLUDE, names = { "UNDEFINED" })
+	public void greaterThanOrEqual(ToastLevel level) {
+		ToastLevelFeedbackMessageFilter filter = ToastLevelFeedbackMessageFilter.greaterThanOrEqual(level);
+
+		Component dummy = new WebMarkupContainer("dummy");
+
+		Integer feedbackMessageLevel = FeedbackMessageLevel.fromToastLevel(level).toInteger();
+		FeedbackMessage argLevelMessage = new FeedbackMessage(dummy, Toast.create(level, "dummy"),
+				feedbackMessageLevel);
+		assertTrue(filter.accept(argLevelMessage), "Argument's level is must contains for accepts level");
+
+		Toast successToast = Toast.success("dummy");
+		FeedbackMessage successMessage = new FeedbackMessage(dummy, successToast, FeedbackMessage.SUCCESS);
+		assertEquals(filter.accept(successMessage),
+				successToast.getToastLevel().greaterThan(level) || successToast.getToastLevel().equals(level));
+
+		Toast infoToast = Toast.info("dummy");
+		FeedbackMessage infoMessage = new FeedbackMessage(dummy, infoToast, FeedbackMessage.INFO);
+		assertEquals(filter.accept(infoMessage),
+				infoToast.getToastLevel().greaterThan(level) || infoToast.getToastLevel().equals(level));
+
+		Toast warnToast = Toast.warn("dummy");
+		FeedbackMessage warnMessage = new FeedbackMessage(dummy, warnToast, FeedbackMessage.WARNING);
+		assertEquals(filter.accept(warnMessage),
+				warnToast.getToastLevel().greaterThan(level) || warnToast.getToastLevel().equals(level));
+
+		Toast errorToast = Toast.error("dummy");
+		FeedbackMessage errorMessage = new FeedbackMessage(dummy, errorToast, FeedbackMessage.ERROR);
+		assertEquals(filter.accept(errorMessage),
+				errorToast.getToastLevel().greaterThan(level) || errorToast.getToastLevel().equals(level));
+
+	}
+
 
 }
