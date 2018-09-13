16fdbfc97205978340dc6a7f4d8feaf0c40fc861
Ryo Tsunoda <try0.development@gmail.com>
2018-09-13 13:08:37 +0900

    Add utility.

--- wicket-toastr-core/src/main/java/jp/try0/wicket/toastr/core/feedback/ToastLevelFeedbackMessageFilter.java
+++ wicket-toastr-core/src/main/java/jp/try0/wicket/toastr/core/feedback/ToastLevelFeedbackMessageFilter.java
@@ -1,10 +1,10 @@
 package jp.try0.wicket.toastr.core.feedback;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.EnumSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.wicket.feedback.FeedbackMessage;
 import org.apache.wicket.feedback.IFeedbackMessageFilter;
@@ -88,42 +88,77 @@ public class ToastLevelFeedbackMessageFilter implements IFeedbackMessageFilter {
 	 */
 	public static ToastLevelFeedbackMessageFilter greaterThanOrEqual(ToastLevel level) {
 
-		Set<ToastLevel> ignores = Sets.newHashSet();
+		Set<ToastLevel> accepts = Sets.newHashSet();
 
 		for (ToastLevel lv : ToastLevel.values()) {
 			if (lv.greaterThan(level) || level == lv) {
-				ignores.add(lv);
+				accepts.add(lv);
 			}
 		}
 
-		return new ToastLevelFeedbackMessageFilter(ignores);
+		return new ToastLevelFeedbackMessageFilter(accepts);
 	}
 
+	/**
+	 * Create filter that accept argument's levels.
+	 *
+	 * @param level
+	 * @return
+	 */
+	public static ToastLevelFeedbackMessageFilter accepts(ToastLevel ...accepts) {
+		return new ToastLevelFeedbackMessageFilter(accepts);
+	}
+
+	/**
+	 * Create filter that ignore argument's levels.
+	 *
+	 * @param level
+	 * @return
+	 */
+	public static ToastLevelFeedbackMessageFilter ignores(ToastLevel ...ignores) {
+
+		Set<ToastLevel> accepts = ToastLevel.asList().stream()
+				.filter(lv -> {
+					for (ToastLevel ignore : ignores) {
+						if (lv == ignore) {
+							return false;
+						}
+					}
+					return true;
+				})
+				.collect(Collectors.toSet());
+
+		return new ToastLevelFeedbackMessageFilter(accepts);
+	}
 
 
 	/**
 	 * Accepts toast levels
 	 */
-	private final EnumSet<ToastLevel> accepts;
+	private final EnumSet<ToastLevel> accepts = EnumSet.noneOf(ToastLevel.class);
 
 
 
 	/**
 	 * Constractor
 	 *
-	 * @param ignores
+	 * @param accepts
 	 */
 	public ToastLevelFeedbackMessageFilter(ToastLevel... accepts) {
-		this(Arrays.asList(accepts));
+		for (ToastLevel level : accepts) {
+			this.accepts.add(level);
+		}
 	}
 
 	/**
 	 * Constractor
 	 *
-	 * @param ignores
+	 * @param accepts
 	 */
 	public ToastLevelFeedbackMessageFilter(Collection<ToastLevel> accepts) {
-		this.accepts = EnumSet.copyOf(accepts);
+		for (ToastLevel level : accepts) {
+			this.accepts.add(level);
+		}
 	}
 
 
